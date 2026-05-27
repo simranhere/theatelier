fcc1e9deb331842b7a3430ea9a399bdb2e689933
simranhere <psingh19_be24@thapar.edu>
2026-05-27 13:03:43 +0530

    fixed react import

--- Front_End/Mern-Project/src/pages/TailorDashboard.tsx
+++ Front_End/Mern-Project/src/pages/TailorDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Scissors, Star, Users, TrendingUp, Award
