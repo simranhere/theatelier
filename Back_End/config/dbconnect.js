547adbf9202fec7c6e33725e680fcd08c68a768f
simranhere <psingh19_be24@thapar.edu>
2026-05-27 13:21:43 +0530

    fixed mongo async

--- Back_End/config/dbconnect.js
+++ Back_End/config/dbconnect.js
@@ -1,15 +1,20 @@
-let mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-function connectToMongoDB() {
-    let url = "mongodb+srv://PrabhsimranSingh:prabh123%40@cluster0.1bz8rt5.mongodb.net/Balaji_Project?retryWrites=true&w=majority";
+async function connectToMongoDB() {
+    try {
 
-    mongoose.connect(url)
-    .then(() => {
+        const url =
+        "mongodb+srv://PrabhsimranSingh:prabh123%40@cluster0.1bz8rt5.mongodb.net/Balaji_Project?retryWrites=true&w=majority";
+
+        await mongoose.connect(url);
+
         console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+
+    } catch (err) {
+
+        console.log("MongoDB Error:", err);
+
+    }
 }
 
 module.exports = { connectToMongoDB };
